f2b14bff3bbf04f57c1e046a2e2d771a15cea63e
Martin Fylke <martin.fylke@volvocars.com>
2019-12-04 15:59:00 +0100

    formatting

--- DayTwo/daytwo.go
+++ DayTwo/daytwo.go
@@ -1,31 +1,31 @@
 package DayTwo
 
 func Solve(startIndex int, input []int) []int {
-		operator := input[startIndex]
+	operator := input[startIndex]
 
-		if operator == 99 {
-			return input
-		}
+	if operator == 99 {
+		return input
+	}
 
-		firstValueIndex := input[startIndex + 1]
-		secondValueIndex := input[startIndex + 2]
-		replacementIndex := input[startIndex + 3]
+	firstValueIndex := input[startIndex+1]
+	secondValueIndex := input[startIndex+2]
+	replacementIndex := input[startIndex+3]
 
-		var returnArray []int
-		switch operator {
-			case 1:
-				additionResult := addValues(input[firstValueIndex], input[secondValueIndex])
-				newArray := replaceValueAtIndex(replacementIndex, additionResult, input)
-				returnArray = Solve(startIndex + 4, newArray)
-				break
-			case 2:
-				multiplicationResult := multiplyValues(input[firstValueIndex], input[secondValueIndex])
-				newArray := replaceValueAtIndex(replacementIndex, multiplicationResult, input)
-				returnArray = Solve(startIndex + 4, newArray)
-				break
-		}
+	var returnArray []int
+	switch operator {
+	case 1:
+		additionResult := addValues(input[firstValueIndex], input[secondValueIndex])
+		newArray := replaceValueAtIndex(replacementIndex, additionResult, input)
+		returnArray = Solve(startIndex+4, newArray)
+		break
+	case 2:
+		multiplicationResult := multiplyValues(input[firstValueIndex], input[secondValueIndex])
+		newArray := replaceValueAtIndex(replacementIndex, multiplicationResult, input)
+		returnArray = Solve(startIndex+4, newArray)
+		break
+	}
 
-		return returnArray
+	return returnArray
 }
 
 func addValues(first int, second int) int {
